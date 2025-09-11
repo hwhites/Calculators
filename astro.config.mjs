069393ba914904277243e3a4df61f8b83c06164c
hwhites <haydenwhiteside272@gmail.com>
2025-09-11 14:46:02 -0500

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
+import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  // This MUST be your final, live domain for the sitemap to work correctly.
+  site: 'https://calccollection.com',
+  
   integrations: [
-    tailwind(),
+    tailwind(), 
+    sitemap(),
     icon({
       include: {
         // Include all icons from the heroicons set
-        heroicons: ['*'],
-      },
-    }),
-  ],
+        heroicons: ['*'] 
+      }
+    })
+  ]
 });
